e57490e030a179478023b81271b3747d44f6b66c
allenkang02 <ksw8841@knou.ac.kr>
2024-09-24 13:41:37 +0900

    서브쿼리와 뷰

---
+++ exercises/05_subquery_exercises.sql
@@ -0,0 +1,14 @@
+-- 1. 가장 높은 학년의 학생 이름 찾기
+SELECT 이름
+FROM 학생
+WHERE 학년 = (SELECT MAX(학년) FROM 학생);
+
+-- 2. '컴퓨터공학' 학과에 속한 학생들의 이름 조회
+SELECT 이름
+FROM 학생
+WHERE 학과 = (SELECT 학과명 FROM 학과 WHERE 학과명 = '컴퓨터공학');
+
+-- 3. 평균 연봉보다 높은 연봉을 받는 직원의 이름과 부서 조회
+SELECT 이름, 부서
+FROM 직원
+WHERE 연봉 > (SELECT AVG(연봉) FROM 직원);
